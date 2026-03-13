6de015f785fca190086b1048370cee7aed32b975
Bozhidar Videnov <bozh.bv03@gmail.com>
2026-03-13 22:38:03 +0200

    Initialized server

---
+++ server/src/main/java/com/zafira/safe_zafira/SafeZafiraApplication.java
@@ -0,0 +1,13 @@
+package com.zafira.safe_zafira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeZafiraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafeZafiraApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/zafira/safe_zafira/SafeZafiraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zafira.safe_zafira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafeZafiraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
