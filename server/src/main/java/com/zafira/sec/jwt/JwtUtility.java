ee8acd68822d0ffd4edfcd3c5f2b805978f6b255
MartinM2304 <m.marinkov6@mail.bg>
2026-03-14 02:14:07 +0200

    use token in prop

--- server/src/main/java/com/zafira/sec/jwt/JwtUtility.java
+++ server/src/main/java/com/zafira/sec/jwt/JwtUtility.java
@@ -2,19 +2,26 @@ package com.zafira.sec.jwt;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
 import java.util.Date;
 
 @Component
-public class JwtUtility {
-	private final String SECRET_KEY = "ZafiraHackathonSuperSecretKeyThatIsVeryLongAndSecure123!";
-	private final SecretKey key = Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+public class JwtUtility
+{
 
+	private final SecretKey key;
 	private final long EXPIRATION_TIME = 1000 * 60 * 60 * 24;
 
-	public String generateToken(long userId, String email) {
+	public JwtUtility(@Value("${jwt.secret}") String secretKey)
+	{
+		this.key = Keys.hmacShaKeyFor(secretKey.getBytes());
+	}
+
+	public String generateToken(long userId, String email)
+	{
 		return Jwts.builder()
 				   .subject(email)
 				   .claim("userId", userId)
