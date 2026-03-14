4eedabf6ad8a27faef4b58d797d252c18ea49253
MartinM2304 <m.marinkov6@mail.bg>
2026-03-14 15:39:35 +0200

    ddz

--- server/src/main/java/com/zafira/SafeZafiraApplication.java
+++ server/src/main/java/com/zafira/SafeZafiraApplication.java
@@ -2,14 +2,10 @@ package com.zafira;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.zafira")
-@EnableJpaRepositories(basePackages = "com.zafira")
-@EntityScan(basePackages = "com.zafira")
 public class SafeZafiraApplication {
 
 	public static void main(String[] args) {
--- server/src/main/java/com/zafira/safe_zafira/telemetry/speed/SpeedLimitService.java
+++ server/src/main/java/com/zafira/safe_zafira/telemetry/speed/SpeedLimitService.java
@@ -1,4 +1,55 @@
 package com.zafira.safe_zafira.telemetry.speed;
 
-public class SpeedLimitService {
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
+
+import java.util.Locale;
+
+@Service
+public class SpeedLimitService
+{
+
+	private final RestClient restClient;
+	private final ObjectMapper objectMapper;
+
+	public SpeedLimitService(ObjectMapper objectMapper)
+	{
+		this.restClient = RestClient.create("https://overpass-api.de/api");
+		this.objectMapper = objectMapper;
+	}
+
+	public Integer getSpeedLimit(double lat, double lon)
+	{
+		String query = String.format(Locale.US,
+									 "[out:json];way(around:30, %f, %f)[\"maxspeed\"];out tags;", lat, lon
+									);
+
+		try
+		{
+			String response = restClient.get()
+										.uri(uriBuilder -> uriBuilder
+												.path("/interpreter")
+												.queryParam("data", query)
+												.build())
+										.retrieve()
+										.body(String.class);
+
+			JsonNode root = objectMapper.readTree(response);
+			JsonNode elements = root.path("elements");
+
+			if (elements.isArray() && !elements.isEmpty())
+			{
+				String maxSpeedRaw = elements.get(0).path("tags").path("maxspeed").asText();
+
+				return Integer.parseInt(maxSpeedRaw.replaceAll("[^0-9]", ""));
+			}
+		}
+		catch (Exception e)
+		{
+			System.err.println("Overpass API failed or parsing error: " + e.getMessage());
+		}
+		return null;
+	}
 }
